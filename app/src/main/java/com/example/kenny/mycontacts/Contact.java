827037335f65945d6153872577a6893507eaf318
Kenny Rozario <Kenny@Rozarios-iMac.local>
2015-04-01 00:07:08 -0400

    First commit

---
+++ app/src/main/java/com/example/kenny/mycontacts/Contact.java
@@ -0,0 +1,17 @@
+package com.example.kenny.mycontacts;
+
+/**
+ * Created by Kenny on 15-03-31.
+ */
+public class Contact {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
