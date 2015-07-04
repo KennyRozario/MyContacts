47c60ddd824e610b9d902285b65934bf0f6cb5b1
Kenny Rozario <Kenny@Kennys-MacBook-Pro.local>
2015-07-04 02:05:31 -0400

    Eighth Commit
    Added the Contact List Fragment and set it up within the activity.

--- app/src/main/java/com/example/kenny/mycontacts/ContactListActivity.java
+++ app/src/main/java/com/example/kenny/mycontacts/ContactListActivity.java
@@ -18,102 +18,17 @@ import java.util.ArrayList;
 
 public class ContactListActivity extends ActionBarActivity {
 
-    private ContactList mContacts;
-    private ContactAdapter mAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
 
-        mContacts = ContactList.getInstance();
-
-        for (int i = 0; i < 30; i++) {
-            Contact contact1 = new Contact();
-            contact1.setName("Kenny Rozario");
-            contact1.emails = new ArrayList<String>();
-            contact1.emails.add("kenny@rozario.com");
-            contact1.phoneNumbers = new ArrayList<String>();
-            contact1.phoneNumbers.add("18000000000");
-            mContacts.add(contact1);
+        if (getFragmentManager().findFragmentById(R.id.list_fragment_container) == null){
+            getFragmentManager().beginTransaction()
+                    .add(R.id.list_fragment_container, new ContactListFragment())
+                    .commit();
         }
 
-        ListView listView = (ListView)findViewById(R.id.contact_list_view);
-        mAdapter = new ContactAdapter(mContacts);
-        listView.setAdapter(mAdapter);
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            int previousFirstItem = 0;
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (firstVisibleItem > previousFirstItem){
-                    getSupportActionBar().hide();
-                }
-                else if (firstVisibleItem < previousFirstItem){
-                    getSupportActionBar().show();
-                }
-
-                previousFirstItem = firstVisibleItem;
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(ContactListActivity.this, ContactViewActivity.class);
-                i.putExtra(ContactViewActivity.EXTRA, position);
-                startActivity(i);
-            }
-        });
     }
 
-    private class ContactAdapter extends ArrayAdapter<Contact>{
-        ContactAdapter(ArrayList<Contact> contacts){
-            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = super.getView(position, convertView, parent);
-
-            Contact contact = getItem(position);
-
-            TextView nameTextView = (TextView)convertView.findViewById(R.id.contact_row_name);
-            nameTextView.setText(contact.getName());
-            return convertView;
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        mAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_contact_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/example/kenny/mycontacts/ContactListFragment.java
@@ -0,0 +1,136 @@
+package com.example.kenny.mycontacts;
+
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContactListFragment extends Fragment {
+
+    private ContactList mContacts;
+    private ContactAdapter mAdapter;
+
+    public ContactListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_contact_list, container, false);
+
+        mContacts = ContactList.getInstance();
+
+        for (int i = 0; i < 30; i++) {
+            Contact contact1 = new Contact();
+            contact1.setName("Kenny Rozario");
+            contact1.emails = new ArrayList<String>();
+            contact1.emails.add("kenny@rozario.com");
+            contact1.phoneNumbers = new ArrayList<String>();
+            contact1.phoneNumbers.add("18000000000");
+            mContacts.add(contact1);
+        }
+
+        ListView listView = (ListView)v.findViewById(R.id.contact_list_view);
+        mAdapter = new ContactAdapter(mContacts);
+        listView.setAdapter(mAdapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            int previousFirstItem = 0;
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                android.support.v7.app.ActionBar ab = ((ActionBarActivity) getActivity()).getSupportActionBar();
+                if (firstVisibleItem > previousFirstItem) {
+                    ab.hide();
+                } else if (firstVisibleItem < previousFirstItem) {
+                    ab.show();
+                }
+
+                previousFirstItem = firstVisibleItem;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getActivity(), ContactViewActivity.class);
+                i.putExtra(ContactViewActivity.EXTRA, position);
+                startActivity(i);
+            }
+        });
+
+        return v;
+    }
+
+    private class ContactAdapter extends ArrayAdapter<Contact> {
+        ContactAdapter(ArrayList<Contact> contacts){
+            super(getActivity(), R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+
+            Contact contact = getItem(position);
+
+            TextView nameTextView = (TextView)convertView.findViewById(R.id.contact_row_name);
+            nameTextView.setText(contact.getName());
+            return convertView;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_contact_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
