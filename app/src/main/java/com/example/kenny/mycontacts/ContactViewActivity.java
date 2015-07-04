bd4a06daa8c57220a4bda2c5bede456fb896088d
Kenny Rozario <Kenny@Kennys-MacBook-Pro.local>
2015-07-04 02:25:51 -0400

    Extra commit
    Converted layout/activity.

---
+++ app/src/main/java/com/example/kenny/mycontacts/ContactViewActivity.java
@@ -0,0 +1,30 @@
+package com.example.kenny.mycontacts;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class ContactViewActivity extends ActionBarActivity {
+
+    public static final String EXTRA = "CVA_Contact";
+
+}
